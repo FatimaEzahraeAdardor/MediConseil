36de03fd6e24b3c6b151d860775f7ef2425f7e1c
FatimaEzahraeAdardor <adardorfatimaezahrae@gmail.com>
2024-12-25 23:10:01 +0100

    first commit

---
+++ src/main/java/org/youcode/mediconseil/MediConseilApplication.java
@@ -0,0 +1,13 @@
+package org.youcode.mediconseil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediConseilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediConseilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/youcode/mediconseil/MediConseilApplicationTests.java
@@ -0,0 +1,13 @@
+package org.youcode.mediconseil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediConseilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
