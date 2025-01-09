10fb78146a2df4d9cf6ba5a4f1a4e922feccf5ef
FatimaEzahraeAdardor <adardorfatimaezahrae@gmail.com>
2025-01-09 04:12:32 +0100

    adding services done

---
+++ src/main/java/org/youcode/mediconseil/service/SpecialityService.java
@@ -0,0 +1,15 @@
+package org.youcode.mediconseil.service;
+import org.springframework.data.domain.Page;
+import org.youcode.mediconseil.domain.Speciality;
+import java.util.Optional;
+import java.util.UUID;
+
+
+public interface SpecialityService {
+    Speciality save(Speciality speciality);
+    Speciality update(Speciality speciality);
+    Boolean delete(UUID id);
+    Optional<Speciality> findById(UUID id);
+    Page<Speciality> getAllSpecialitiesPaginated(int page, int size);
+
+}
