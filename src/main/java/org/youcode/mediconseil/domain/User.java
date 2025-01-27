f3d6b8a5dc402e77b845a1d8136d1b83a5d34787
FatimaEzahraeAdardor <adardorfatimaezahrae@gmail.com>
2025-01-27 19:58:01 +0100

    added the permissions for roles

--- src/main/java/org/youcode/mediconseil/domain/User.java
+++ src/main/java/org/youcode/mediconseil/domain/User.java
@@ -2,8 +2,11 @@ package org.youcode.mediconseil.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.youcode.mediconseil.domain.enums.Role;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,7 +17,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
@@ -35,4 +38,33 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<Consultation> consultations ;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getAuthorities();
+    }
+
+    @Override
+    public String getUsername() {
+        return emailAddress;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/org/youcode/mediconseil/domain/enums/Role.java
+++ src/main/java/org/youcode/mediconseil/domain/enums/Role.java
@@ -1,7 +1,73 @@
 package org.youcode.mediconseil.domain.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.youcode.mediconseil.domain.enums.Permission.*;
+
+@RequiredArgsConstructor
 public enum Role {
-    ADMIN,
-    PATIEN,
-    DOCTOR
+    PATIENT(
+            Set.of(
+                    USER_BOOK_APPOINTMENT,
+                    USER_VIEW_APPOINTMENT_DETAILS,
+                    USER_CANCEL_APPOINTMENT,
+                    USER_RESCHEDULE_APPOINTMENT,
+                    USER_VIEW_DOCTOR_PROFILES,
+                    USER_RECEIVE_CONSULTATION_DOCUMENT,
+                    USER_READ_HEALTH_ARTICLES,
+                    USER_MANAGE_PERSONAL_INFO,
+                    USER_VIEW_NOTIFICATIONS,
+                    USER_VIEW_CONSULTATION_HISTORY
+            )
+    ),
+
+    DOCTOR(
+            Set.of(
+                    DOCTOR_VIEW_APPOINTMENTS,
+                    DOCTOR_CONFIRM_APPOINTMENT,
+                    DOCTOR_CANCEL_APPOINTMENT,
+                    DOCTOR_RESCHEDULE_APPOINTMENT,
+                    DOCTOR_VIEW_PATIENT_DETAILS,
+                    DOCTOR_PROVIDE_CONSULTATION_DOCUMENT,
+                    ADMIN_MANAGE_HEALTH_ARTICLES,
+                    DOCTOR_MANAGE_AVAILABILITY,
+                    DOCTOR_VIEW_SCHEDULE,
+                    DOCTOR_MANAGE_PROFILE,
+                    DOCTOR_VIEW_STATISTICS
+            )
+    ),
+
+    ADMIN(
+            Set.of(
+                    ADMIN_MANAGE_APPOINTMENTS,
+                    ADMIN_VIEW_APPOINTMENT_DETAILS,
+                    ADMIN_CANCEL_APPOINTMENT,
+                    ADMIN_RESCHEDULE_APPOINTMENT,
+                    ADMIN_VIEW_DOCTOR_SCHEDULES,
+                    ADMIN_MANAGE_DOCTOR_AVAILABILITY,
+                    ADMIN_MANAGE_HEALTH_ARTICLES,
+                    ADMIN_PUBLISH_HEALTH_ARTICLES,
+                    ADMIN_MANAGE_USERS,
+                    ADMIN_MANAGE_DOCTORS,
+                    ADMIN_VIEW_STATISTICS,
+                    ADMIN_MANAGE_NOTIFICATIONS
+            )
+    );
+
+    @Getter
+    private final Set<Permission> permissions;
+
+    public List<SimpleGrantedAuthority> getAuthorities() {
+        var authorities = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.name()))
+                .collect(Collectors.toList());
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + name()));
+        return authorities;
+    }
 }
