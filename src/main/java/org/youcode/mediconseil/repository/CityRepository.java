7ef92b925c9ec97c4f2dc06f06e1f3646611b3d6
fatimaezahraeadardor <fz.adardor@enova.ma>
2025-03-27 23:48:15 +0000

    get city list and filtred by region

--- src/main/java/org/youcode/mediconseil/repository/CityRepository.java
+++ src/main/java/org/youcode/mediconseil/repository/CityRepository.java
@@ -1,6 +1,9 @@
 package org.youcode.mediconseil.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.youcode.mediconseil.domain.City;
 
 import java.util.List;
@@ -8,8 +11,9 @@ import java.util.UUID;
 
 public interface CityRepository extends JpaRepository<City, UUID> {
     boolean existsByName(String name);
-    List<City> findByRegion(String region);
-
+    Page<City> findByRegion(String region , Pageable pageable);
+    @Query("SELECT DISTINCT c.region FROM City c ORDER BY c.region ASC")
+    List<String> findDistinctRegions();
 
 
 }
